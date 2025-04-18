7f73fd2d39d39eb01f1a4fb7c9f58afb499e3997
larisa.pasca <larisapasca29@gmail.com>
2025-04-18 19:31:25 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.pt2025_30422_larisa_pasca_assignment_3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pt2025_30422_larisa_pasca_assignment_3 to javafx.fxml;
+    exports com.example.pt2025_30422_larisa_pasca_assignment_3;
+}
